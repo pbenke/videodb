f173a79f0f656ac433c1cce0081e5af58de0a189
Klaus Christiansen <klaus_edwin@hotmail.com>
2022-07-08 16:15:10 +0200

    updated dependencies.

--- vendor/composer/include_paths.php
+++ vendor/composer/include_paths.php
@@ -6,9 +6,9 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    $vendorDir . '/pear/pear_exception',
     $vendorDir . '/pear/console_getopt',
-    $vendorDir . '/pear/pear-core-minimal/src',
     $vendorDir . '/pear/ole',
+    $vendorDir . '/pear/pear-core-minimal/src',
+    $vendorDir . '/pear/pear_exception',
     $vendorDir . '/pear/spreadsheet_excel_writer',
 );
