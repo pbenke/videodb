bcbce752ec9ccf1fe63f5d3556985e0d169ec2ab
Petar Benke <4955050+pbenke@users.noreply.github.com>
2022-12-01 20:35:40 +0100

    chore: setting non-default user agent for HTTP client #3

--- core/httpclient.php
+++ core/httpclient.php
@@ -100,7 +100,13 @@ function get_response_encoding($response)
 function httpClient($url, $cache = false, $para = null, $reload = false)
 {
     global $config;
-    $client = new GuzzleHttp\Client();
+
+    $clientConfig = [
+        'headers' => [
+            'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
+        ],
+    ];
+    $client = new GuzzleHttp\Client($clientConfig);
 
     $requestConfig = [];
     $headers = '';  // additional HTTP headers, used for post data
