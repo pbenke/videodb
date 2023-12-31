dddaa2a5bf639da1940029f19d4afbcdd223957e
Klaus Christiansen <klaus_edwin@hotmail.com>
2023-12-31 12:03:18 +0100

    Added tv series to dvdfr and promoted engine to stable

--- engines/dvdfr.php
+++ engines/dvdfr.php
@@ -25,29 +25,8 @@ $GLOBALS['dvdfrIdPrefix'] = 'dvdfr:';
  */
 function dvdfrMeta(): array
 {
-    return array('name' => 'Dvdfr (fr)', 'stable' => 0, 'capabilities' => array('movie', 'image'));
+    return array('name' => 'Dvdfr (fr)', 'stable' => 1, 'capabilities' => array('movie', 'image'));
 }
-
-/**
- * Clean a string
- *
- * @param   string    The string to clean
- * @return  string    The cleaned string
- */
-function dvdfrCleanStr(string $str): string
-{
-    // Remove spaces
-    $str = trim($str);
-
-    // Translate strange (MS-Word?) quotes
-    $str = preg_replace('/&#039;/', "'", $str);
-
-    // Remove HTML entities
-    $str = html_entity_decode($str);
-
-    return $str;
-}
-
 
 /**
  * Get Url to visit Dvdfr for a specific movie
@@ -63,7 +42,6 @@ function dvdfrContentUrl($id): string
 
     return $dvdfrServer.'/api/dvd.php?id='.$dvdfrID;
 }
-
 
 /**
  * Get Url to search Dvdfr for a movie
@@ -149,16 +127,20 @@ function dvdfrData($imdbID): array
     $movie = new SimpleXMLElement($resp['data']);
 
     $title = dvdfrSplitTitle((string) $movie->titres->fr);
-    $data['title']      = $title['title'];
-    $data['subtitle']   = $title['subtitle'];
-    $data['origtitle']  = $movie->titres->vo;
+    $data['title'] = $title['title'];
+    $data['subtitle'] = $title['subtitle'];
+    $data['origtitle'] = $movie->titres->vo;
 
 //    $data['language'] = does not exist on dvdfr
-    $data['year']      = $movie->annee;
-    $data['coverurl']  = $movie->cover;
-    $data['runtime']   = $movie->duree;
-    $data['rating']    = $movie->critiques->public;
-    $data['plot']      = $movie->synopsis;
+    $data['year'] = $movie->annee;
+    if ($movie->annee == 0) {
+        $data['year'] = $movie->annee['debut'];
+    }
+
+    $data['coverurl'] = $movie->cover;
+    $data['runtime'] = $movie->duree;
+    $data['rating'] = $movie->critiques->public;
+    $data['plot'] = $movie->synopsis;
 
     $countries = [];
     foreach ($movie->listePays->pays as $country) {
@@ -178,136 +160,162 @@ function dvdfrData($imdbID): array
     $data['director'] = implode(', ', $directors);
     $data['cast'] = implode("\n", $actors);
 
+    foreach ($movie->categories->categorie as $category) {
+        $genreId = (int) $category['id'];
+        if (dvdfrIsTv($genreId)) {
+            $data['istv'] = 1;
+        }
+        $genre = dvdfrGetGenre($genreId);
+        if ($genre) {
+            $data['genres'][] = $genre;
+        }
+    }
+
+    return $data;
+}
+
+function dvdfrIsTv(int $genreId): bool
+{
+// 57  => "Mini-series / Feuilletons",
+// 58  => "Série TV",
+// 59  => "Sitcom",
+// 60  => "Série anime / OAV",
+// 61  => "Autres séries",
+// 75  => "Série d'animation enfants",
+// 100 => "Série format court",
+// 107 => "Telenovela",
+// 108 => "Série documentaire",
+    return in_array($genreId, [57, 58, 59, 60, 61, 75, 100, 107, 108]);
+}
+
+function dvdfrGetGenre(int $genreId): string {
+
     // maps dvdfr category ids to videodb category names
     /*
-     Modify	id	name
-     edit	1	Action
-     edit	2	Adventure
-     edit	3	Animation
-     edit	4	Comedy
-     edit	5	Crime
-     edit	6	Documentary
-     edit	7	Drama
-     edit	8	Family
-     edit	9	Fantasy
-     edit	10	Film-Noir
-     edit	11	Horror
-     edit	12	Musical
-     edit	13	Mystery
-     edit	14	Romance
-     edit	15	Sci-Fi
-     edit	16	Short
-     edit	17	Thriller
-     edit	18	War
-     edit	19	Western
-     edit	20	Adult
-     edit	21	Music
-     edit	22	Biography
-     edit	23	History
-     edit	24	Sport
+    Genre:
+    Action
+    Adult
+    Adventure
+    Animation
+    Biography
+    Comedy
+    Crime
+    Documentary
+    Drama
+    Family
+    Fantasy
+    Film-Noir
+    History
+    Horror
+    Music
+    Musical
+    Mystery
+    Romance
+    Sci-Fi
+    Short
+    Sport
+    Thriller
+    War
+    Western
     */
 
     $category_map = [
         1  =>  "Action",
         2  =>  "Animation",
-        3  =>  "Adventure", //"Aventure",
-        4  =>  "Comedy", //"Comédie",
-        5  =>  "Comedy", //"Comédie dramatique",
-        6  =>  "Comedy", //"Comédie musicale",
-        7  =>  "Music", //"Concert",
-        8  =>  "Conte",
-        9  =>  "Court métrage",
-        10  => "Culture",
-        11  => "Danse",
-        12  => "Divers",
-        13  => "Documentaire",
-        14  => "Drame",
-        15  => "Adult", //"Erotique",
-        16  => "Espionnage",
-        17  => "Fantastique",
-        18  => "Guerre",
-        19  => "Music", //"Hard Rock / Métal",
-        20  => "Historique",
-        21  => "Horreur",
-        22  => "Comedy", //"Humour",
-        23  => "Japanimation",
-        24  => "Adult", //"Hentai / Japanimation érotique",
-        25  => "Music", //"Jazz / Blues",
-        26  => "Music", //"Karaoké",
-        27  => "Kung Fu",
-        28  => "Méthode",
-        29  => "Muet",
-        30  => "Musical",
-        31  => "Music", //"Opéra",
-        32  => "Music", //"Musique classique",
-        33  => "Péplum",
-        34  => "Policier",
-        54  => "Enfants / Famille",
-        55  => "Music", //"Rap",
-        56  => "Sci-Fi", //"Science-fiction",
-        57  => "Mini-series / Feuilletons",
-        58  => "Série TV",
-        59  => "Sitcom",
-        60  => "Série anime / OAV",
-        61  => "Autres séries",
-        62  => "Spectacle",
+        3  =>  "Adventure",   // "Aventure",
+        4  =>  "Comedy",      // "Comédie",
+        5  =>  "Comedy",      // "Comédie dramatique",
+        6  =>  "Comedy",      // "Comédie musicale",
+        7  =>  "Music",       // "Concert",
+        8  =>  "",            // "Conte",
+        9  =>  "Short",       // "Court métrage",
+        10  => "",            // "Culture",
+        11  => "",            // "Danse",
+        12  => "",            // "Divers",
+        13  => "Documentary", // "Documentaire",
+        14  => "Drama",       // "Drame",
+        15  => "Adult",       // "Erotique",
+        16  => "Thriller",    // "Espionnage",
+        17  => "Fantasy",     // "Fantastique",
+        18  => "War",         // "Guerre",
+        19  => "Music",       // "Hard Rock / Métal",
+        20  => "History",     // "Historique",
+        21  => "Horror",      // "Horreur",
+        22  => "Comedy",      // "Humour",
+        23  => "Animation",   // "Japanimation",
+        24  => "Adult",       // "Hentai / Japanimation érotique",
+        25  => "Music",       // "Jazz / Blues",
+        26  => "Music",       // "Karaoké",
+        27  => "Action",      // "Kung Fu",
+        28  => "",            // "Méthode",
+        29  => "",            // "Muet",
+        30  => "Musical",     // "Musical",
+        31  => "Music",       // "Opéra",
+        32  => "Music",       // "Musique classique",
+        33  => "",            // "Péplum",
+        34  => "",            // "Policier",
+        54  => "Family",      // "Enfants / Famille",
+        55  => "Music",       // "Rap",
+        56  => "Sci-Fi",      // "Science-fiction",
+        57  => "",            // "Mini-series / Feuilletons",
+        58  => "",            // "Série TV",
+        59  => "",            // "Sitcom",
+        60  => "Animation",   // "Série anime / OAV",
+        61  => "",            // "Autres séries",
+        62  => "",            // "Spectacle",
         63  => "Sport",
-        64  => "Music", //"Techno / Electro",
+        64  => "Music",       // "Techno / Electro",
         65  => "Theatre",
         66  => "Thriller",
-        67  => "Variété française",
-        68  => "Variété internationale",
-        69  => "Voyages",
+        67  => "",            // "Variété française",
+        68  => "",            // "Variété internationale",
+        69  => "",            // "Voyages",
         70  => "Western",
-        71  => "Musiques du monde",
-        72  => "Beaux-arts",
-        73  => "Emotion",
-        74  => "Comédie romantique",
-        75  => "Série d'animation enfants",
-        76  => "Music", //"R&amp;B / Soul",
-        78  => "Univers LGBT",
-        79  => "Jeux",
-        81  => "Bollywood",
-        82  => "Sports mécaniques",
-        83  => "Catch",
-        85  => "Santé / Bien-être",
-        86  => "Animaux",
-        87  => "Culture urbaine",
-        88  => "Société",
-        89  => "Ambiance / Relaxation",
-        90  => "Science / Découvertes",
-        91  => "Cuisine / Jardinage / Déco",
-        92  => "Chasse / Pêche",
+        71  => "Music",       // "Musiques du monde",
+        72  => "",            // "Beaux-arts",
+        73  => "",            // "Emotion",
+        74  => "Comedy",      // "Comédie romantique",
+        75  => "Animation",   // "Série d'animation enfants",
+        76  => "Music",       // "R&amp;B / Soul",
+        78  => "",            // "Univers LGBT",
+        79  => "",            // "Jeux",
+        81  => "",            // "Bollywood",
+        82  => 'Sport',       // "Sports mécaniques",
+        83  => "",            // "Catch",
+        85  => "",            // "Santé / Bien-être",
+        86  => "Animation",   // "Animaux",
+        87  => "",            // "Culture urbaine",
+        88  => "",            // "Société",
+        89  => "",            // "Ambiance / Relaxation",
+        90  => "Documentary", // "Science / Découvertes",
+        91  => "",            // "Cuisine / Jardinage / Déco",
+        92  => "Nature",      // "Chasse / Pêche",
         93  => "Nature",
-        94  => "Football",
-        95  => "Documentaire-fiction",
-        96  => "Kickboxing / Freefight",
-        97  => "Fan Service",
-        98  => "Gore",
-        99  => "Biopic / Biographie / Histoire vraie",
-        100 => "Série format court",
-        101 => "Musique de films",
-        102 => "Catastrophe",
+        94  => "Sport",       // "Football",
+        95  => "",            // "Documentaire-fiction",
+        96  => "Sport",       // "Kickboxing / Freefight",
+        97  => "",            // "Fan Service",
+        98  => "Horror",      // "Gore",
+        99  => "Biography",   // "Biopic / Biographie / Histoire vraie",
+        100 => "Short",       // "Série format court",
+        101 => "Music",       // "Musique de films",
+        102 => "",            // "Catastrophe",
         103 => "Romance",
-        104 => "Anime Yaoi",
-        105 => "Documentaire musical",
-        106 => "Emissions TV",
-        107 => "Telenovela",
-        108 => "Série documentaire",
-        111 => "Cinéma expérimental",
-        112 => "Super-héros",
-        114 => "Parodie",
-        117 => "Spiritualité / Religion",
-        109 => "Caritatif",
-        110 => "Moyen métrage",
+        104 => "Animation",   // "Anime Yaoi",
+        105 => "Documentary", // "Documentaire musical",
+        106 => "",            // "Emissions TV",
+        107 => "Drama",       // "Telenovela",
+        108 => "Documentary", // "Série documentaire",
+        111 => "",            // "Cinéma expérimental",
+        112 => "",            // "Super-héros",
+        114 => "Comedy",      // "Parodie",
+        117 => "",            // "Spiritualité // Religion",
+        109 => "",            // "Caritatif",
+        110 => "",            // "Moyen métrage",
         113 => "Fantasy",
-        115 => "Court et moyen métrage documentaire"];
+        115 => "Documentary"]; // "Court et moyen métrage documentaire"];
 
-    foreach ($movie->categories->categorie as $category) {
-        $data['genres'][] = $category_map[(int) $category['id']];
-    }
-
-    return $data;
+    return $category_map[$genreId];
 }
 
 function dvdfrSplitTitle(string $title): array
@@ -350,6 +358,7 @@ function dvdfrActorUrl(string $name, string $id): string
  */
 function dvdfrActor($name, $actorengineid): array
 {
+    global $dvdfrServer;
     global $cache;
 
     $url = dvdfrActorUrl($name, $actorengineid);
@@ -364,7 +373,11 @@ function dvdfrActor($name, $actorengineid): array
 
     $ary = [];
     if (preg_match('/<div id="starPicture" .+? src="(.+?)"/si', $resp['data'], $m)) {
-        $ary[0][0] = '/stars/s4028-scarlett-johansson.html';
+
+        // actor url
+        preg_match('#'.$dvdfrServer.'(.+)#i', $url, $actorUrl);
+        $ary[0][0] = $actorUrl[1];
+
         if (!strstr($m[1], 'nophoto.jpg')) {
             $ary[0][1] = $m[1]; // img url
         }
--- test/engines/TestDvdfr.php
+++ test/engines/TestDvdfr.php
@@ -55,18 +55,59 @@ class TestDVDFR extends TestCase
 		$this->assertEquals('4,53', $data['rating']);
 		$this->assertEquals('George Lucas', $data['director']);
 		$this->assertEquals('USA', $data['country']);
+		$this->assertArrayNotHasKey('istv', $data);
+
+		$this->assertCount(2, $data['genres']);
 		$this->assertContains('Sci-Fi', $data['genres']);
 		$this->assertContains('Adventure', $data['genres']);
 
-		$this->assertMatchesRegularExpression('/Liam Neeson.*Ewan McGregor.*Natalie Portman/s', $data['cast']);
+		$this->assertStringContainsString('Liam Neeson::::dvdfr:10977', $data['cast']);
+		$this->assertStringContainsString('Ewan McGregor::::dvdfr:9192', $data['cast']);
+		$this->assertStringContainsString('Natalie Portman::::dvdfr:6177', $data['cast']);
+
 		$this->assertMatchesRegularExpression("/Bloqués sur la planète Tatooine après avoir secouru la Reine Amidala \(Natalie Portman\), l'apprenti Jedi Obi-Wan Kenobi \(Ewan McGregor\)/", $data['plot']);
+	}
+
+	function testTvSerie(): void
+	{
+		// Terminator - The Sarah Connor Chronicles - Saison 1 (2008) - Blu-ray
+		// https://www.dvdfr.com/dvd/f151323-terminator-the-sarah-connor-chronicles-saison-1.html
+		// https://www.dvdfr.com/dvd/dvd.php?id=151323
+		$id = 'dvdfr:151323';
+
+		$data = engineGetData($id, 'dvdfr', false);
+
+		$this->assertNotCount(0, $data);
+
+//		var_dump($data);
+
+		$this->assertEquals('Terminator', $data['title']);
+		$this->assertEquals('The Sarah Connor Chronicles - Saison 1', $data['subtitle']);
+		$this->assertEquals('Terminator: The Sarah Connor Chronicles', $data['origtitle']);
+		$this->assertArrayNotHasKey('language', $data);
+		$this->assertEquals(2008, $data['year']);
+		$this->assertEquals('https://www.dvdfr.com/images/dvd/covers/100x140/c336f6fca8cf8bab696dcdef453a80f0/151323/old-terminator_the_sarah_connor_chronicles_saison_1_br.0.jpg', $data['coverurl']);
+		$this->assertEquals(487, $data['runtime']);
+		$this->assertEquals('4,00', $data['rating']);
+		$this->assertStringContainsString('David Nutter', $data['director']);
+		$this->assertEquals('USA', $data['country']);
+		$this->assertEquals(1, $data['istv']);
+
+		$this->assertCount(1, $data['genres']);
+		$this->assertContains('Sci-Fi', $data['genres']);
+
+		$this->assertStringContainsString('Thomas Dekker::::dvdfr:47911', $data['cast']);
+		$this->assertStringContainsString('Summer Glau::::dvdfr:70828', $data['cast']);
+		$this->assertStringContainsString('Lena Headey::::dvdfr:13572', $data['cast']);
+
+		$this->assertMatchesRegularExpression("/Après être venus à bout du Terminator, Sarah Connor et son fils John/", $data['plot']);
 	}
 
 	function testMovieWithSubTitle(): void
 	{
-		// Star Wars: Episode I
-		// https://www.dvdfr.com/dvd/f2869_star_wars_-_episode_i_-_la_menace_fantome.html
-		// https://www.dvdfr.com/dvd/dvd.php?id=2869
+		// Mobile Fighter G Gundam
+		// https://www.dvdfr.com/dvd/f171716-mobile-fighter-g-gundam-premiere-partie.html
+		// https://www.dvdfr.com/dvd/dvd.php?id=171716
 		$id = 'dvdfr:171716';
 
 		$data = engineGetData($id, 'dvdfr', false);
@@ -78,6 +119,8 @@ class TestDVDFR extends TestCase
 		$this->assertEquals('Mobile Fighter G Gundam', $data['title']);
 		$this->assertEquals('Première partie', $data['subtitle']);
 		$this->assertEquals('Kidô butôden ji Gandamu', $data['origtitle']);
+		$this->assertEquals(1994, $data['year']);
+		$this->assertEquals(625, $data['runtime']);
 	}
 
 	function testMovieWithMultipleCounties(): void
@@ -96,6 +139,7 @@ class TestDVDFR extends TestCase
 		$this->assertEmpty($data['origtitle']);
 		$this->assertArrayNotHasKey('language', $data);
 		$this->assertEmpty($data['rating']);
+		$this->assertCount(1, $data['genres']);
 		$this->assertContains('Comedy', $data['genres']);
 		$this->assertEquals('Nouvelle-Zélande, USA, République tchèque', $data['country']);
 	}
@@ -142,6 +186,16 @@ class TestDVDFR extends TestCase
 
         $this->assertEquals('/stars/s4028-scarlett-johansson.html', $data[0][0]);
         $this->assertEquals("https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Scarlett_Johansson_by_Gage_Skidmore_2_%28cropped%29.jpg/600px-Scarlett%20Johansson%20by%20Gage%20Skidmore%202%20%28cropped%29.jpg", $data[0][1]);
+	}
+
+	function testEngineActorWithoutImage(): void
+	{
+	    $name = 'Tintrinai Thikhasuk';
+	    $id = 'dvdfr:201155';
+	    $data = engineActor($name, $id, 'dvdfr');
+
+        $this->assertEquals('/stars/s201155-tintrinai-thikhasuk.html', $data[0][0]);
+        $this->assertEmpty($data[0][1]);
 	}
 }
 
